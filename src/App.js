574a7e1fc5f454e9761eaeaaae463d01de7a6127
Sofia Tovar <d015yyqb3@mozmail.com>
2023-11-18 17:58:38 -0500

    Se contruyeron las bases, y se configuro next-ui y tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@nextui-org/react';
+import './Assets/css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Button color='primary' >Hola</Button> 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +16,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
     </div>
   );
 }
