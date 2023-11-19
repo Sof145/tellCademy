f1d725f4d70c8d887fb4185b6f0d529c9c12c1db
Sofia Tovar <d015yyqb3@mozmail.com>
2023-11-18 22:33:35 -0500

    Se agrego el componente Footer

---
+++ src/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <footer className='mt-20 space-y-4 p-8' >
+
+        <p className='text-danger' >tellcademy@.com</p>
+        <p><p>&#169;2023 TellCademy</p></p>
+
+    </footer>
+  )
+}
